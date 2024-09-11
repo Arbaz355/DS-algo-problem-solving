cfb5dbe7388f01414ea059d3f5bbdfad01c941fe
Arbaz355 <arbazakhter355@gmail.com>
2024-09-11 10:53:38 +0530

    array question solved

---
+++ mergeSortArray.js
@@ -0,0 +1,44 @@
+/* You are given a list of sorted non-descending integer arrays, write a function to merge them into one sorted non-descending array.
+Question link:- https://bigfrontend.dev/problem/merge-sorted-arrays
+*/
+
+/**
+ * @param {number[][]} arrList
+ * non-descending integer array
+ * @return {number[]}
+ */
+
+const merge = (arr) => {
+    // base case 
+    if(arr.length <= 0){
+        return [];
+    }
+
+    if(arr.length === 1){
+        return [...arr[0]]
+    }
+
+    const flattedArray = [];
+
+    for(let i=0; i<arr.length; i++){
+        flattedArray.push(...arr[i])
+    }
+    flattedArray.sort((a, b) => a - b);
+
+    return flattedArray;
+}
+
+
+// driver code
+const result = merge([
+  [1, 1, 1, 100, 1000, 10000],
+  [1, 2, 2, 2, 200, 200, 1000],
+  [1000000, 10000001],
+  [2, 3, 3],
+]);
+
+// output -> [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 100 , 200, 200, 1000, 1000, 10000, 1000000, 10000001]
+// 1) empty return empty array
+// 2) single item return the 0th index and flatted value
+// 3) main logic -> flat -> sort  
+console.log(result);
